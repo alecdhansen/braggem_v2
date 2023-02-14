7d7ebb0978d0b62c9f110fd689d464abdad81269
Alec Hansen <alecdhansen@gmail.com>
2023-02-14 12:15:15 -0500

    ts config

--- frontend/static/vite.config.ts
+++ frontend/static/vite.config.ts
@@ -4,4 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "/frontend/static/",
+    manifest: true,
+    rollupOptions: {
+      input: {
+        key: "static/",
+      },
+    },
+  },
 });
