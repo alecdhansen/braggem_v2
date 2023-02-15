02b273dde4421e5dc1719defda36ea2dc2dbb26a
Alec Hansen <alecdhansen@gmail.com>
2023-02-15 15:02:22 -0500

    add chakraui dependency

--- frontend/static/src/navigation/ProtectedRoute/index.tsx
+++ frontend/static/src/navigation/ProtectedRoute/index.tsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 
 export const ProtectedRoute = ({ children }: any) => {
-  const { user } = useAuth();
+  const { user }: any = useAuth();
   if (!user) {
     return <Navigate to="/" />;
   }
