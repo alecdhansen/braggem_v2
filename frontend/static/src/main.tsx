17125e99182f3f1f5164f755ffce8b134914087d
Alec Hansen <alecdhansen@gmail.com>
2023-02-15 13:46:34 -0500

    fix app import

--- frontend/static/src/main.tsx
+++ frontend/static/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "vite/modulepreload-polyfill";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import App from "./components/App";
+import App from "./App";
 import { AuthProvider } from "./hooks/useAuth";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
