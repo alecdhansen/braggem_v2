b4f2724a9d4b7fdccbffd3afbf93e17c05dee101
Alec Hansen <alecdhansen@gmail.com>
2023-02-23 12:38:13 -0500

    load in readied content in app

--- frontend/static/src/App/index.tsx
+++ frontend/static/src/App/index.tsx
@@ -1,15 +1,15 @@
 import { useAuth } from "../hooks/useAuth";
 import { Routes, Route } from "react-router-dom";
-// import { ProtectedLayout } from "./ProtectedLayout";
+import { ProtectedLayout } from "../navigation/ProtectedLayout";
 // import Card from "../Card/Card";
-// import UnprotectedLayout from "./UnprotectedLayout";
+import UnprotectedLayout from "../navigation/UnprotectedLayout";
 // import LoginHome from "../Login/LoginHome";
 import Fof from "../components/404Page";
 // import Register from "../Login/Register";
 // import ProfilePage from "../Profile/ProfilePage";
 // import Leaderboard from "../Leaderboard/LeaderBoard";
 // import HeadToHead from "../HeadToHead/HeadToHead";
-// import UserInfo from "../Users/UserInfo";
+import UserInfo from "../components/UserInfo";
 
 const App = () => {
   const { user }: any = useAuth();
@@ -17,17 +17,17 @@ const App = () => {
   return (
     <>
       <Routes>
-        {/* <Route element={<UnprotectedLayout />}>
-          <Route path="/" element={<LoginHome />} />
-          <Route path="/register" element={<Register />} />
-        </Route> */}
-        {/* <Route path="/home" element={<ProtectedLayout />}>
-          <Route path="games" element={<Card />} />
+        <Route element={<UnprotectedLayout />}>
+          {/* <Route path="/" element={<LoginHome />} />
+          <Route path="/register" element={<Register />} /> */}
+        </Route>
+        <Route path="/home" element={<ProtectedLayout />}>
+          {/* <Route path="games" element={<Card />} />
           <Route path={user?.username} element={<ProfilePage />} />
           <Route path="leaderboard" element={<Leaderboard />} />
-          <Route path="headtohead" element={<HeadToHead />} />
+          <Route path="headtohead" element={<HeadToHead />} /> */}
           <Route path=":username" element={<UserInfo />} />
-        </Route> */}
+        </Route>
         <Route path="*" element={<Fof />} />
       </Routes>
     </>
