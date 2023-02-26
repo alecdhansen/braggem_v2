1fd662dfe84fd05cbd91e86886d2bd47700bab48
Alec Hansen <alecdhansen@gmail.com>
2023-02-26 12:24:37 -0500

    change footerprops and add cardprops

--- frontend/static/src/components/HomePage/types.ts
+++ frontend/static/src/components/HomePage/types.ts
@@ -1,7 +1,17 @@
-export type Props = {
+export type FooterProps = {
   todaysGames?: any;
   todaysPicks?: any;
   picksCompletedPercentage?: any;
   getTodaysPicks?: any;
   timeUntilEstGameInMS?: any;
+};
+
+export type CardProps = {
+  todaysGames?: any;
+  handleSubmit?: any;
+  handleAwayTeamInput?: any;
+  handleHomeTeamInput?: any;
+  gameID?: any;
+  awayTeamFocus?: any;
+  homeTeamFocus?: any;
 };
