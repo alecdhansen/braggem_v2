366419e8bbd9d07f5be23d0350f5e72b61cd4730
Alec Hansen <alecdhansen@gmail.com>
2023-02-27 16:49:40 -0500

    headtohead activated

--- frontend/static/src/App/index.tsx
+++ frontend/static/src/App/index.tsx
@@ -8,7 +8,7 @@ import Fof from "../components/404Page";
 import Register from "../components/Login/Register";
 // import ProfilePage from "../Profile/ProfilePage";
 // import Leaderboard from "../Leaderboard/LeaderBoard";
-// import HeadToHead from "../HeadToHead/HeadToHead";
+import HeadToHead from "../components/HeadToHead";
 import UserInfo from "../components/UserInfo";
 
 const App = () => {
@@ -24,8 +24,8 @@ const App = () => {
         <Route path="/home" element={<ProtectedLayout />}>
           <Route path="games" element={<Card />} />
           {/* <Route path={user?.username} element={<ProfilePage />} />
-          <Route path="leaderboard" element={<Leaderboard />} />
-          <Route path="headtohead" element={<HeadToHead />} /> */}
+          <Route path="leaderboard" element={<Leaderboard />} /> */}
+          <Route path="headtohead" element={<HeadToHead />} />
           <Route path=":username" element={<UserInfo />} />
         </Route>
         <Route path="*" element={<Fof />} />
