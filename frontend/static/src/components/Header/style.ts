824d982923faaa9d1d9c6141ca76715a81cb413b
Alec Hansen <alecdhansen@gmail.com>
2023-03-12 13:28:46 -0400

    take off vh on header

--- frontend/static/src/components/Header/style.ts
+++ frontend/static/src/components/Header/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   position: relative;
   width: 100vw;
-  height: 100vh;
+  /* height: 100vh; */
 `;
 
 export const CurveHead = styled.div.attrs(() => ({
